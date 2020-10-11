64aab0b07fa95a96176de5c0af849b8e18356541
serhii-serdiuk <sergiy_serduk@i.ua>
2020-10-12 01:53:40 +0300

    HW3

--- api/contacts/contacts.model.js
+++ api/contacts/contacts.model.js
@@ -17,7 +17,7 @@ const contactSchema = new Schema({
     type: String,
     validate: {
       validator: function (v) {
-        return /\d{3}-\d{3}-\d{2}-\d{2}/.test(v);
+        return /\d{3}-\d{3}-\d{4}/.test(v);
       },
       message: (props) => `${props.value} is not a valid phone number!`,
     },
