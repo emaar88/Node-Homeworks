a9be6d1c2ec88b22ee315fb2195c19eea1caae79
serhii-serdiuk <sergiy_serduk@i.ua>
2020-10-15 15:20:13 +0300

    Master HW1-3

--- start.js
+++ start.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-const ContactService = require("./api/server");
-=======
-const ContactService = require("./api/contactsWithoutDB/server/server");
->>>>>>> c8b51618083af6fd85999da3da2c698f64571d36
+const ContactService = require("./api/contactsMongoose/server/server");
 
 new ContactService().start();
